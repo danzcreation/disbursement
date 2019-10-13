58a4b62091fb83794e83a75884487fe19acd3d4d
Dandy Derisetiya Hairawan <danzcreation@gmail.com>
2019-10-13 11:05:21 +0700

    finalize disburse model and index/main file

--- app/models/disburse.php
+++ app/models/disburse.php
@@ -1,25 +1,83 @@
 <?php
     require 'db/connection.php';
+    require 'app/services/sbig-api.php';
 
     class Disburse {
         function __construct() {
             $this->connection = Connection::getInstance();
         }
 
-        function all() {
-            $statement = $this->connection->prepare('SELECT * FROM `disburses`');
-            $statement->execute();
-            return $statement->fetchAll();
+        function create($params) {
+            echo "[INFO] submitting data to API ...\n";
+            $api = new SBigAPI();
+            $api_result = json_decode($api->disburse_create($params), true);
+
+            if (strtotime($api_result['time_served']) < 0) {
+                unset($api_result['time_served']);
+            }
+
+            return $this->save_data($api_result);
         }
 
-        function find($id) {
+        function find($id, $get_update = true) {
             $sql = "SELECT * FROM disburses WHERE id = :id";
 
             $statement = $this->connection->prepare($sql);
             $statement->bindParam(':id', $id, PDO::PARAM_STR);
             $statement->execute();
 
-            return $statement->fetch();
+            $result = $statement->fetch();
+
+            if ($result['status'] != 'SUCCESS' && $get_update) {
+                $result = $this->update_from_api($result['id']);
+            }
+
+            return $result;
+        }
+
+        private function update_from_api($id) {
+            echo "[INFO] updating data from API ...\n";
+            $api = new SBigAPI();
+            $api_result = json_decode($api->disburse_detail($id), true);
+
+            if ($api_result['status'] == 'SUCCESS') {
+                // only update status, receipt and time served value
+                $selected_attributes = array_flip(array('id', 'status', 'receipt', 'time_served'));
+                $params = array_intersect_key($api_result, $selected_attributes);
+
+                return $this->update_data($params);
+            } else {
+                return $this->find($id, false);
+            }
+        }
+
+        private function save_data($disburse) {
+            $sql = sprintf(
+                "INSERT INTO disburses (%s) values (%s)",
+                implode(", ", array_keys($disburse)),
+                ':'. implode(", :", array_keys($disburse))
+            );
+
+            $statement = $this->connection->prepare($sql);
+            $statement->execute($disburse);
+
+            return $this->find($disburse['id'], false);
+        }
+
+        private function update_data($disburse) {
+            foreach($disburse as $key => $value) {
+                $columns[] = $key .' = :'. $key;
+            }
+
+            $sql = sprintf(
+                "UPDATE disburses SET %s, updated_at = CURRENT_TIMESTAMP WHERE id = :id",
+                implode(", ", array_values($columns))
+            );
+
+            $statement = $this->connection->prepare($sql);
+            $statement->execute($disburse);
+
+            return $this->find($disburse['id'], false);
         }
     }
 ?>
---
+++ index.php
@@ -0,0 +1,90 @@
+<?php
+    require 'app/models/disburse.php';
+
+    function main_menu() {
+        echo "---------------------\n";
+        echo "1 - Get Disbursion\n";
+        echo "2 - Submit Disbursion\n";
+        echo "0 - Quit\n\n";
+        echo "Enter your choice: ";
+    }
+
+    function show_disburse() {
+        echo "Please enter transaction ID: ";
+        $id = trim(fgets(STDIN));
+
+        $disburse = new Disburse();
+        $result = $disburse->find($id);
+
+        disburse_presenter($result);
+    }
+
+    function create_disburse() {
+        $input_values = array();
+
+        echo "Please enter bank code: ";
+        $input_values['bank_code'] = trim(fgets(STDIN));
+
+        echo "Please enter account number: ";
+        $input_values['account_number'] = trim(fgets(STDIN));
+
+        echo "Please enter amount: ";
+        $input_values['amount'] = trim(fgets(STDIN));
+
+        echo "Please enter remark: ";
+        $input_values['remark'] = trim(fgets(STDIN));
+
+        $disburse = new Disburse();
+        $result = $disburse->create($input_values);
+
+        disburse_presenter($result);
+    }
+
+    function disburse_presenter($disburse) {
+        echo "---------------------\n";
+        echo('ID               : '. $disburse['id'] ."\n");
+        echo('Bank Code        : '. $disburse['bank_code'] ."\n");
+        echo('Account Number   : '. $disburse['account_number'] ."\n");
+        echo('Amount           : '. $disburse['amount'] ."\n");
+        echo('Fee              : '. $disburse['fee'] ."\n");
+        echo('Beneficiary Name : '. $disburse['beneficiary_name'] ."\n");
+        echo('Status           : '. $disburse['status'] ."\n");
+        echo('Receipt          : '. $disburse['receipt'] ."\n");
+        echo('Remark           : '. $disburse['remark'] ."\n");
+        echo('Timestamp        : '. $disburse['timestamp'] ."\n");
+        echo('Time Served      : '. $disburse['time_served'] ."\n");
+        echo('Created At       : '. $disburse['created_at'] ."\n");
+        echo('Updated At       : '. $disburse['updated_at'] ."\n");
+    }
+
+
+    echo "[Disbursion System]\n";
+    do {
+        // Print the menu on console
+        main_menu();
+
+        // Read user choice
+        $choice = trim(fgets(STDIN));
+
+        // Act based on user choice
+        switch($choice) {
+        
+            case 1: {
+                show_disburse();
+                break;
+            }
+            case 2: {
+                create_disburse();
+                break;
+            }
+            case 0: {
+                break;
+            }
+            default: {
+                echo "\n\nPlease provide a valid choice\n";
+            }
+        }
+    } while ($choice != 0);
+
+    exit(0);
+?>
